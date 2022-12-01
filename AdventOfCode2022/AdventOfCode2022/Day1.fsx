546e5300598171598c078c52f7806245af74d106
Florian Ockhuysen <F.Ockhuysen@ccv.eu>
2022-12-01 23:27:14 +0100

    day 1 solved

---
+++ AdventOfCode2022/AdventOfCode2022/Day1.fsx
@@ -0,0 +1,48 @@
+﻿open System.IO
+open System
+
+let analyse (problem: string) = problem.Split("\n\n") |> Array.map (fun l -> l.Split('\n'))
+
+let sr = new StreamReader("inputs/input_day01.txt")
+let fileContents = sr.ReadToEnd()
+let nums = fileContents |> analyse
+
+let caloriesForElves (nums: string[][]) =
+    nums
+    |> Array.map(fun calories ->
+        calories
+        |> Array.map(Int32.Parse)
+        |> Array.sum
+    )
+
+let solve1 (cals: string[][]) =
+    caloriesForElves cals
+    |> Array.max
+
+let solve2 (cals: string[][]) =
+    caloriesForElves cals
+    |> Array.sortDescending
+    |> Array.take(3)
+    |> Array.sum
+
+let raw_test = @"1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000"
+let test = raw_test |> analyse
+
+
+printfn $"first test answer is %d{solve1 test} (should be 24000)"
+printfn $"first answer is %d{solve1 nums}"
+printfn $"second test answer is %d{solve2 test} (should be 45000)"
+printfn $"second answer is %d{solve2 nums}"
