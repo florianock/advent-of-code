4c2c42ad91bfa7414594d6eb2e4771ac7502a1e1
Florian Ockhuysen <F.Ockhuysen@ccv.eu>
2022-12-02 17:51:40 +0100

    refactored a bit

--- AdventOfCode2022/src/Day02/Day02.fs
+++ AdventOfCode2022/src/Day02/Day02.fs
@@ -10,21 +10,22 @@ let toIndexTuples (shapes: seq<string[]>) =
             Array.findIndex (fun elem -> elem = s[1]) [|"X"; "Y"; "Z"|]
         ))
 
+let solve input scoreFunction =
+    input
+    |> toIndexTuples
+    |> Seq.map scoreFunction
+    |> Seq.sum
+
+
 let getResultScore shapeIndexes =
     match shapeIndexes with
-    | 0, 1 -> 6
-    | 1, 2 -> 6
-    | 2, 0 -> 6
-    | 0, 0 -> 3
-    | 1, 1 -> 3
-    | 2, 2 -> 3
+    | o, p when p = (o + 1) % 3 -> 6
+    | o, p when o = p -> 3
     | _, _ -> 0
 
 let solvePart1 (input: seq<string[]>) =
-    input
-    |> toIndexTuples
-    |> Seq.map (fun (other, player) -> 1 + player + getResultScore (other, player))
-    |> Seq.sum
+    solve input (fun (other, player) -> 1 + player + getResultScore (other, player))
+
 
 let getPlayerShapeIndex (other, result) =
     if result = 0 then (other + 2) % 3
@@ -32,7 +33,4 @@ let getPlayerShapeIndex (other, result) =
     else other
 
 let solvePart2 (input: seq<string[]>) =
-    input
-    |> toIndexTuples
-    |> Seq.map (fun (other, result) -> 1 + getPlayerShapeIndex (other, result) + result * 3)
-    |> Seq.sum
+    solve input (fun (other, result) -> 1 + getPlayerShapeIndex (other, result) + result * 3)
