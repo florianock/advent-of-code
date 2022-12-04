f12e0ba2194efab3d2bcfdd825af70467d308287
Florian Ockhuysen <F.Ockhuysen@ccv.eu>
2022-12-04 12:18:14 +0100

    Yup, Day04 done with more Set logic

--- AdventOfCode2022/Day04/Day04.fs
+++ AdventOfCode2022/Day04/Day04.fs
@@ -1,7 +1,21 @@
 ﻿module Day04
 
-let preprocess (puzzle: string) : seq<string> = puzzle.TrimEnd().Split("\n")
+let expandSections (x: string) =
+    let list = x.Split("-") |> Array.map System.Int32.Parse
+    seq { list[0] .. list[1] } |> Set.ofSeq
 
-let solvePart1 (input: seq<string>) = 0
+let processPair (line: string) =
+    let pairs = line.Split(",") |> Array.map expandSections
+    (pairs[0], pairs[1])
 
-let solvePart2 (input: seq<string>) = 0
+let preprocess (puzzle: string) = puzzle.TrimEnd().Split("\n") |> Array.map processPair
+
+let solvePart1 (input: (Set<int> * Set<int>)[]) =
+    input
+    |> Array.filter (fun (a, b) -> a.IsSubsetOf(b) || a.IsSupersetOf(b))
+    |> Array.length
+
+let solvePart2 (input: (Set<int> * Set<int>)[]) =
+    input
+    |> Array.filter (fun (a, b) -> not(a.Equals(a - b) && b.Equals(b - a)))
+    |> Array.length
--- AdventOfCode2022/Day04/Day04Tests.fs
+++ AdventOfCode2022/Day04/Day04Tests.fs
@@ -3,17 +3,22 @@
 open NUnit.Framework
 open Day04
 
-let test = @"
+let test = @"2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8
 "
 
 [<Test>]
 let solvePart1 () =
     let input = test |> preprocess
     let actual = solvePart1 input
-    Assert.AreEqual(0, actual)
+    Assert.AreEqual(2, actual)
 
 [<Test>]
 let solvePart2 () =
     let input = test |> preprocess
     let actual = solvePart2 input
-    Assert.AreEqual(0, actual)
+    Assert.AreEqual(4, actual)
