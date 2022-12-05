f4e88b4a338cf1c52b559e885d36c6b6a56ef278
Florian Ockhuysen <F.Ockhuysen@ccv.eu>
2022-12-05 23:38:03 +0100

    Adjusted test readability

--- AdventOfCode2022/Day05/Day05Tests.fs
+++ AdventOfCode2022/Day05/Day05Tests.fs
@@ -34,61 +34,128 @@ let getTopCratesBasic () =
 
 [<Test>]
 let getTopCratesMulti () =
-    let input = [| [| "X"; "A" |]; [| "X"; "X"; "B" |]; [| "X"; "X"; "X"; "C" |] |]
+    let input =
+        [|
+            [| "X"; "A" |]
+            [| "X"; "X"; "B" |]
+            [| "X"; "X"; "X"; "C" |]
+        |]
     let actual = getTopCrates input
     Assert.AreEqual("ABC", actual)
 
 [<Test>]
 let applyInstructionTest () =
-    let stack = [| [| "X"; "X" |]; [| "X"; "B"; "A"; "X" |]; [| "X"; "X"; "X"; "C" |] |]
+    let stack =
+        [|
+            [| "X"; "X" |]
+            [| "X"; "B"; "A"; "X" |]
+            [| "X"; "X"; "X"; "C" |]
+        |]
     let instruction = (2, 2, 1)
     let actual = applyInstruction false stack instruction
     Assert.AreEqual(
-        [| [| "X"; "X"; "X"; "A" |]; [| "X"; "B" |]; [| "X"; "X"; "X"; "C" |] |],
+        [|
+            [| "X"; "X"; "X"; "A" |]
+            [| "X"; "B" |]
+            [| "X"; "X"; "X"; "C" |]
+        |],
         actual)
 
 [<Test>]
 let applyInstructionTestMultipleCratesAtOnce () =
-    let stack = [| [| "X"; "X" |]; [| "X"; "B"; "X"; "A" |]; [| "X"; "X"; "X"; "C" |] |]
+    let stack =
+        [|
+            [| "X"; "X" |]
+            [| "X"; "B"; "X"; "A" |]
+            [| "X"; "X"; "X"; "C" |]
+        |]
     let instruction = (2, 2, 1)
     let actual = applyInstruction true stack instruction
     Assert.AreEqual(
-        [| [| "X"; "X"; "X"; "A" |]; [| "X"; "B" |]; [| "X"; "X"; "X"; "C" |] |],
+        [|
+            [| "X"; "X"; "X"; "A" |]
+            [| "X"; "B" |]
+            [| "X"; "X"; "X"; "C" |]
+        |],
         actual)
 
 [<Test>]
 let followInstructionsTestBasic () =
-    let stack = [| [| "X"; "X" |]; [| "X"; "B"; "A" |]; [| "X"; "X"; "X"; "C" |] |]
+    let stack =
+        [|
+            [| "X"; "X" |]
+            [| "X"; "B"; "A" |]
+            [| "X"; "X"; "X"; "C" |]
+        |]
     let instructions = seq { "move 1 from 2 to 1" }
     let actual = followInstructions false (instructions, stack)
     Assert.AreEqual(
-        [| [| "X"; "X"; "A" |]; [| "X"; "B" |]; [| "X"; "X"; "X"; "C" |] |],
+        [|
+            [| "X"; "X"; "A" |]
+            [| "X"; "B" |]
+            [| "X"; "X"; "X"; "C" |]
+        |],
         actual)
 
 [<Test>]
 let followInstructionsTestMoreMoves () =
-    let stack = [| [| "X"; "X" |]; [| "X"; "A" |]; [| "X"; "X"; "X"; "C"; "B"; "X"; "X" |] |]
-    let instructions = seq { "move 1 from 2 to 1"; "move 3 from 3 to 2" }
+    let stack =
+        [|
+            [| "X"; "X" |]
+            [| "X"; "A" |]
+            [| "X"; "X"; "X"; "C"; "B"; "X"; "X" |]
+        |]
+    let instructions =
+        seq {
+            "move 1 from 2 to 1"
+            "move 3 from 3 to 2"
+        }
     let actual = followInstructions false (instructions, stack)
     Assert.AreEqual(
-        [| [| "X"; "X"; "A" |]; [| "X"; "X"; "X"; "B" |]; [| "X"; "X"; "X"; "C" |] |],
+        [|
+            [| "X"; "X"; "A" |]
+            [| "X"; "X"; "X"; "B" |]
+            [| "X"; "X"; "X"; "C" |]
+        |],
         actual)
 
 [<Test>]
 let followInstructionsTestMoreMovesAndMultipleCratesAtOnce () =
-    let stack = [| [| "X"; "X" |]; [| "X"; "A" |]; [| "X"; "X"; "X"; "C"; "B"; "X"; "X" |] |]
-    let instructions = seq { "move 1 from 2 to 1"; "move 3 from 3 to 2" }
+    let stack =
+        [|
+            [| "X"; "X" |]
+            [| "X"; "A" |]
+            [| "X"; "X"; "X"; "C"; "X"; "X"; "B" |]
+        |]
+    let instructions =
+        seq {
+            "move 1 from 2 to 1"
+            "move 3 from 3 to 2"
+        }
     let actual = followInstructions true (instructions, stack)
     Assert.AreEqual(
-        [| [| "X"; "X"; "A" |]; [| "X"; "B"; "X"; "X" |]; [| "X"; "X"; "X"; "C" |] |],
+        [|
+            [| "X"; "X"; "A" |]
+            [| "X"; "X"; "X"; "B" |]
+            [| "X"; "X"; "X"; "C" |]
+        |],
         actual)
 
 [<Test>]
 let parseTest () =
     let actualInstructions, actualStack = test |> readLines |> parse
     Assert.AreEqual(
-        seq { "move 1 from 2 to 1"; "move 3 from 1 to 3"; "move 2 from 2 to 1"; "move 1 from 1 to 2" },
+        seq {
+            "move 1 from 2 to 1"
+            "move 3 from 1 to 3"
+            "move 2 from 2 to 1"
+            "move 1 from 1 to 2"
+        },
         actualInstructions)
     Assert.AreEqual(
-        [| [| "Z"; "N" |]; [| "M"; "C"; "D" |]; [| "P" |] |],
+        [|
+            [| "Z"; "N" |]
+            [| "M"; "C"; "D" |]
+            [| "P" |]
+        |],
         actualStack)
