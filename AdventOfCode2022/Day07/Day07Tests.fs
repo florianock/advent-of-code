7e27044bb3da767059a103eaf3a5693a3ab33d08
Florian Ockhuysen <F.Ockhuysen@ccv.eu>
2022-12-07 08:55:35 +0100

    Day07 setup

---
+++ AdventOfCode2022/Day07/Day07Tests.fs
@@ -0,0 +1,41 @@
+﻿module Day07Tests
+
+open NUnit.Framework
+open Day07
+
+let test = @"$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k
+"
+
+[<Test>]
+let solvePart1 () =
+    let input = test |> preprocess
+    let actual = solvePart1 input
+    Assert.AreEqual(95437, actual)
+
+[<Test>]
+let solvePart2 () =
+    let input = test |> preprocess
+    let actual = solvePart2 input
+    Assert.AreEqual(0, actual)
