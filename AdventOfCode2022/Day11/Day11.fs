d02090ca7fa92cf6ec87c782b72f6e7dbd1463e4
Florian Ockhuysen <F.Ockhuysen@ccv.eu>
2022-12-11 09:45:57 +0100

    Day11 Setup

---
+++ AdventOfCode2022/Day11/Day11.fs
@@ -0,0 +1,9 @@
+﻿module Day11
+
+// --- Day 11: Monkey in the Middle ---
+
+let preprocess (puzzle: string) : seq<string> = puzzle.TrimEnd().Split ("\n")
+
+let solvePart1 (input: seq<string>) = 0
+
+let solvePart2 (input: seq<string>) = 0
---
+++ AdventOfCode2022/Day11/Day11Tests.fs
@@ -0,0 +1,46 @@
+﻿module Day11Tests
+
+open NUnit.Framework
+open Day11
+
+let test =
+    @"Monkey 0:
+  Starting items: 79, 98
+  Operation: new = old * 19
+  Test: divisible by 23
+    If true: throw to monkey 2
+    If false: throw to monkey 3
+
+Monkey 1:
+  Starting items: 54, 65, 75, 74
+  Operation: new = old + 6
+  Test: divisible by 19
+    If true: throw to monkey 2
+    If false: throw to monkey 0
+
+Monkey 2:
+  Starting items: 79, 60, 97
+  Operation: new = old * old
+  Test: divisible by 13
+    If true: throw to monkey 1
+    If false: throw to monkey 3
+
+Monkey 3:
+  Starting items: 74
+  Operation: new = old + 3
+  Test: divisible by 17
+    If true: throw to monkey 0
+    If false: throw to monkey 1
+"
+
+[<Test>]
+let solvePart1 () =
+    let input = test |> preprocess
+    let actual = solvePart1 input
+    Assert.AreEqual (10605, actual)
+
+[<Test>]
+let solvePart2 () =
+    let input = test |> preprocess
+    let actual = solvePart2 input
+    Assert.AreEqual (0, actual)
