d35cbd0c465796b29431a37b930aceb8a588472b
Florian Ockhuysen <F.Ockhuysen@ccv.eu>
2022-12-13 22:50:13 +0100

    Learned about fparsec, thanks to Jo van Eyck

--- AdventOfCode2022/Day13/Day13Tests.fs
+++ AdventOfCode2022/Day13/Day13Tests.fs
@@ -32,10 +32,10 @@ let test =
 let solvePart1 () =
     let input = test |> preprocess
     let actual = solvePart1 input
-    Assert.AreEqual (0, actual)
+    Assert.AreEqual (13, actual)
 
 [<Test>]
 let solvePart2 () =
     let input = test |> preprocess
     let actual = solvePart2 input
-    Assert.AreEqual (0, actual)
+    Assert.AreEqual (140, actual)
