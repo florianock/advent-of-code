ad123710bd3591dd0cafd2cd479307397970f4f0
Florian Ockhuysen <F.Ockhuysen@ccv.eu>
2022-12-18 20:34:47 +0100

    Setup day 19

---
+++ AdventOfCode2022/Day19/Day19Tests.fs
@@ -0,0 +1,19 @@
+﻿module Day19Tests
+
+open NUnit.Framework
+open Day19
+
+let test = @"
+"
+
+[<Test>]
+let solvePart1 () =
+    let input = test |> preprocess
+    let actual = solvePart1 input
+    Assert.AreEqual(0, actual)
+
+[<Test>]
+let solvePart2 () =
+    let input = test |> preprocess
+    let actual = solvePart2 input
+    Assert.AreEqual(0, actual)
