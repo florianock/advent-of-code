1d9d777c3e89671ae2a784909f6c23a2471c4008
Florian <florian@zocalo-ict.nl>
2022-12-18 10:38:00 +0100

    Added day18 setup

---
+++ AdventOfCode2022/Day18/Day18.fs
@@ -0,0 +1,16 @@
+﻿module Day18
+
+// --- Day 18: Boiling Boulders ---
+
+let preprocess (puzzle: string) : (int * int * int) list =
+    puzzle.TrimEnd().Split("\n")
+    |> Array.map (fun c ->
+        c.Split(',')
+        |> function
+            | [| x; y; z |] -> (int x, int y, int z)
+            | p -> failwith $"Unable to interpret coordinates {p}")
+    |> Array.toList
+
+let solvePart1 input = 0
+
+let solvePart2 input = 0
