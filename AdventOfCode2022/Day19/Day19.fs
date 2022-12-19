9b041baf7f2f55c1cb204e7ea8864d3335dd9cb4
Florian Ockhuysen <F.Ockhuysen@ccv.eu>
2022-12-19 19:31:44 +0100

    day19

--- AdventOfCode2022/Day19/Day19.fs
+++ AdventOfCode2022/Day19/Day19.fs
@@ -2,8 +2,12 @@
 
 // --- Day 19: Not Enough Minerals ---
 
-let preprocess (puzzle: string) : seq<string> = puzzle.TrimEnd().Split "\n"
+let preprocess (puzzle: string) : seq<string> = puzzle.TrimEnd().Split "\n\n"
 
-let solvePart1 (input: seq<string>) = 0
+let getQualityLevel blueprint = blueprint |> String.length
+
+let solvePart1 (blueprints: seq<string>) =
+    // answer is blueprints |> Seq.map (qualitylevel  * blueprint ID) |> Seq.sum
+    blueprints |> Seq.map getQualityLevel |> Seq.sum
 
 let solvePart2 (input: seq<string>) = 0
