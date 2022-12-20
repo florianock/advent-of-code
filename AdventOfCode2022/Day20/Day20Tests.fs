1523c1ebab390bd84aa9fcc884dc4d2d1125d3c6
Florian Ockhuysen <F.Ockhuysen@ccv.eu>
2022-12-20 12:47:06 +0100

    added Day20

---
+++ AdventOfCode2022/Day20/Day20Tests.fs
@@ -0,0 +1,25 @@
+﻿module Day20Tests
+
+open NUnit.Framework
+open Day20
+
+let test =
+    @"1
+2
+-3
+3
+-2
+0
+4"
+
+[<Test>]
+let solvePart1 () =
+    let input = test |> preprocess
+    let actual = solvePart1 input
+    Assert.AreEqual (3, actual)
+
+[<Test>]
+let solvePart2 () =
+    let input = test |> preprocess
+    let actual = solvePart2 input
+    Assert.AreEqual (0, actual)
