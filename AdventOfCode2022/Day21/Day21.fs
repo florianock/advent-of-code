7e9d626cd5fa745658548d9ff11d6f2925caed2b
Florian Ockhuysen <F.Ockhuysen@ccv.eu>
2022-12-23 00:20:11 +0100

    Day 21 done

--- AdventOfCode2022/Day21/Day21.fs
+++ AdventOfCode2022/Day21/Day21.fs
@@ -2,27 +2,34 @@
 
 // --- Day 21: Monkey Math ---
 
-open Checked
+// open Checked
+
+type Operator =
+    | Subtract
+    | Add
+    | Divide
+    | Multiply
 
 type MonkeyShout =
-    | Primitive of uint64
-    | Operation of (uint64 -> uint64 -> uint64) * string * string
+    | Primitive of int64
+    | Operation of Operator * string * string
+    | X
 
 let (|Int|_|) str =
-    match System.Int32.TryParse (str: string) with
-    | true, i -> Some (uint64 i)
+    match System.Int64.TryParse (str: string) with
+    | true, i -> Some i
     | _ -> None
 
 let (|Calculation|_|) (str: string) =
-    let parts = str.Split (' ')
+    let parts = str.Split ' '
 
     match parts with
     | [| monkeyA; op; monkeyB |] ->
         match op with
-        | "-" -> Some ((-), monkeyA, monkeyB)
-        | "+" -> Some ((+), monkeyA, monkeyB)
-        | "/" -> Some ((/), monkeyA, monkeyB)
-        | "*" -> Some ((*), monkeyA, monkeyB)
+        | "-" -> (Subtract, monkeyA, monkeyB) |> Some
+        | "+" -> (Add, monkeyA, monkeyB) |> Some
+        | "/" -> (Divide, monkeyA, monkeyB) |> Some
+        | "*" -> (Multiply, monkeyA, monkeyB) |> Some
         | _ -> failwith $"Unknown operator {op}"
     | _ -> None
 
@@ -42,15 +49,76 @@ let preprocess (puzzle: string) =
     |> Array.map (fun m -> m.Split (": ") |> parseMonkey)
     |> Map
 
+let getOperator (op: Operator) =
+    match op with
+    | Add -> (+)
+    | Subtract -> (-)
+    | Divide -> (/)
+    | Multiply -> (*)
 
-let solvePart1 (primates: Map<string, MonkeyShout>) =
-    let rec go (simian: string) (lookup: Map<string, MonkeyShout>) =
-        let result = lookup[simian]
-
-        match result with
+let solve monkeys monkey =
+    let rec go (lookup: Map<string, MonkeyShout>) (simian: string) =
+        match lookup[simian] with
         | Primitive i -> i
-        | Operation (op, simianA, simianB) -> op (go simianA lookup) (go simianB lookup)
+        | Operation (op, simianA, simianB) -> (getOperator op) (go lookup simianA) (go lookup simianB)
+        | X -> failwith "Can only solve for Primitive and Operation. You should use solveForX"
 
-    go "root" primates
+    go monkeys monkey
 
-let solvePart2 (input: Map<string, MonkeyShout>) = 0
+let solvePart1 (primates: Map<string, MonkeyShout>) = solve primates "root"
+
+let trySolve monkeys monkey =
+    try
+        (solve monkeys monkey) |> Some
+    with _ ->
+        None
+
+let inline getGoalForX (goal: int64) (operator: Operator, left: int64 option, right: int64 option) =
+    if left.IsNone then
+        let value = right.Value
+
+        match operator with
+        | Subtract -> goal + value
+        | Add -> goal - value
+        | Divide -> goal * value
+        | Multiply -> goal / value
+    else
+        let value = left.Value
+
+        match operator with
+        | Subtract -> value - goal
+        | Add -> goal - value
+        | Divide -> value / goal
+        | Multiply -> goal / value
+
+let solveForX (goal: int64) (monkeys: Map<string, MonkeyShout>) (monkey: string) =
+    let rec loop g (ms: Map<string, MonkeyShout>) m =
+        let shout = ms[m]
+
+        match shout with
+        | X -> g
+        | Operation (op, l, r) ->
+            let maybeLeft = trySolve ms l
+            let maybeRight = trySolve ms r
+            let newGoal = getGoalForX g (op, maybeLeft, maybeRight)
+            let direction = if maybeLeft.IsSome then r else l
+            loop newGoal ms direction
+        | Primitive i -> failwith $"Can't solve a Primitive for X! {i}; monkey {m}"
+
+    loop goal monkeys monkey
+
+let solvePart2 (wrongInput: Map<string, MonkeyShout>) =
+    let left, right =
+        match wrongInput["root"] with
+        | Operation (_, l, r) -> l, r
+        | _ -> failwith "this root is no good!"
+
+    let input = wrongInput |> Map.remove "root" |> Map.add "humn" X
+
+    let leftOption = trySolve input left
+    let rightOption = trySolve input right
+
+    if leftOption.IsSome then
+        solveForX leftOption.Value input right
+    else
+        solveForX rightOption.Value input left
--- AdventOfCode2022/Day21/Day21Tests.fs
+++ AdventOfCode2022/Day21/Day21Tests.fs
@@ -31,3 +31,24 @@ let solvePart2 () =
     let input = test |> preprocess
     let actual = solvePart2 input
     Assert.AreEqual (301, actual)
+
+
+// root: pppw = sjmn    (4 + (2 * (x - 3))) / 4 = 150
+
+// left = (4 + (2 * (x - 3))) / 4 = 150
+// pppw: cczh / lfqf    (4 + (2 * (x - 3))) / 4 = 150 -> 150 * 4
+// cczh: sllz + lgvd    4 + (2 * (x - 3)) = 600 -> 600 - 4
+// lfqf: 4
+// sllz: 4
+// lgvd: ljgn * ptdq    2 * (x - 3) = 596 -> 596 / 2
+// ljgn: 2
+// ptdq: humn - dvpt    x - 3 = 298 -> 298 + 3 = 301 __x = 301__
+// humn: 5
+// dvpt: 3
+
+// right = 150
+// sjmn: drzm * dbpl    30 * 5
+// drzm: hmdt - zczc    32 - 2
+// dbpl: 5
+// hmdt: 32
+// zczc: 2
