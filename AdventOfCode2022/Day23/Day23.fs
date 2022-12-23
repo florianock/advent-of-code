9580c785bec64595dcff8512aba3d63059493ceb
Florian Ockhuysen <F.Ockhuysen@ccv.eu>
2022-12-23 22:31:33 +0100

    day23 wip

--- AdventOfCode2022/Day23/Day23.fs
+++ AdventOfCode2022/Day23/Day23.fs
@@ -2,8 +2,33 @@
 
 // --- Day 23: Unstable Diffusion ---
 
+type Direction =
+    | N
+    | NE
+    | E
+    | SE
+    | S
+    | SW
+    | W
+    | NW
+
 let preprocess (puzzle: string) : seq<string> = puzzle.TrimEnd().Split "\n"
 
 let solvePart1 (input: seq<string>) = 0
 
 let solvePart2 (input: seq<string>) = 0
+
+let test =
+    @"....#..
+..###.#
+#...#.#
+.#...##
+#.###..
+##.#.##
+.#..#.."
+
+let field = test.TrimEnd().Split "\n" |> Array.toList
+let preferredDirections = [| N; S; E; W; N; S; E; W; N; S |] 
+
+field
+|> List.
