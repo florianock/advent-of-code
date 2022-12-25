3d6f91dcd28ca1764cb19a065f920bade34ebbc8
Florian Ockhuysen <F.Ockhuysen@ccv.eu>
2022-12-25 22:48:25 +0100

    Eliminated the mutable variables

--- AdventOfCode2022/Day25/Day25.fs
+++ AdventOfCode2022/Day25/Day25.fs
@@ -2,47 +2,11 @@
 
 // --- Day 25: Full of Hot Air ---
 
-open Checked
+open Snafu
 
 let preprocess (puzzle: string) : seq<string> = puzzle.TrimEnd().Split "\n"
 
-module Snafu =
-    let toDecimal number =
-        if number |> String.length < 1 then
-            failwith $"NaN {number}"
-
-        number
-        |> Seq.rev
-        |> Seq.mapi (fun i c ->
-            match c with
-            | '2'
-            | '1'
-            | '0' -> (pown 5L i) * (int64 (c.ToString ()))
-            | '-' -> (pown 5L i) * -1L
-            | '=' -> (pown 5L i) * -2L
-            | _ -> failwith $"Wrong input {c}")
-        |> Seq.sum
-
-    let fromDecimal dec =
-        let chars = [| "0"; "1"; "2"; "="; "-" |]
-
-        let inline getChar n =
-            if 0L <= n && n <= 2L then (chars[int n], 0L)
-            elif n = 3L || n = 4L then (chars[int n], 5L)
-            else failwith $"Unable to process number {n}"
-
-        let mutable result = ""
-        let mutable num = dec
-
-        while num > 0L do
-            let n = num % 5L
-            let newN, extra = getChar n
-            result <- result + newN
-            num <- (num + extra) / 5L
-
-        result |> Seq.rev |> Seq.map string |> Seq.fold (+) ""
-
-let solvePart1 (input: seq<string>) =
-    input |> Seq.sumBy Snafu.toDecimal |> Snafu.fromDecimal
+let solvePart1 (snafuNumbers: seq<string>) =
+    snafuNumbers |> Seq.sumBy toDecimal |> fromDecimal
 
 let solvePart2 (input: seq<string>) = "tbd"
---
+++ AdventOfCode2022/Day25/Snafu.fs
@@ -0,0 +1,37 @@
+﻿module Snafu
+
+let chars = [| "="; "-"; "0"; "1"; "2" |]
+
+let toDecimal (number: string) : int64 =
+    if number |> String.length < 1 then
+        failwith $"NaN {number}"
+
+    number
+    |> Seq.rev
+    |> Seq.mapi (fun i c ->
+        match c with
+        | '2'
+        | '1'
+        | '0' -> (c |> string |> int64) * (pown 5L i)
+        | '-' -> -1L * (pown 5L i)
+        | '=' -> -2L * (pown 5L i)
+        | _ -> failwith $"Wrong input {c}")
+    |> Seq.sum
+
+let fromDecimal (dec: int64) : string =
+    let inline getChar n =
+        let p = int (n + 2L) % 5
+
+        if 0L <= n && n <= 2L then (chars[p], 0L)
+        elif n = 3L || n = 4L then (chars[p], 5L)
+        else failwith $"Unable to process number {n}"
+
+    let rec loop n acc =
+        match n with
+        | 0L -> acc
+        | num ->
+            let rem = num % 5L
+            let c, carry = getChar rem
+            acc + c + (loop ((num + carry) / 5L) acc)
+
+    (loop dec "") |> Seq.rev |> Seq.map string |> Seq.fold (+) ""
